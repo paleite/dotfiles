a0fce73bf0545fe18f6e6df029a2799368a9a5f2
Patrick Eriksson <paleite@kth.se>
2019-03-19 13:50:15 +0100

    Added plugins for oh-my-zsh

---
+++ .oh-my-zsh/custom/plugins.zsh
@@ -0,0 +1,10 @@
+# Load plugins
+plugins=(git zsh-autosuggestions zsh-syntax-highlighting wd)
+
+for plugin ($plugins); do
+  if [ -f $ZSH_CUSTOM/plugins/$plugin/$plugin.plugin.zsh ]; then
+    source $ZSH_CUSTOM/plugins/$plugin/$plugin.plugin.zsh
+  elif [ -f $ZSH/plugins/$plugin/$plugin.plugin.zsh ]; then
+    source $ZSH/plugins/$plugin/$plugin.plugin.zsh
+  fi
+done
