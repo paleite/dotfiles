231f9fd64efcb2eb8d84f3e9dee3ca655571748f
Patrick Eriksson <paleite@kth.se>
2019-03-24 04:38:47 +0100

    Fix headless xcode-install (NOTE: tested on 10.14)

--- dotfiles/xcode-install.sh
+++ dotfiles/xcode-install.sh
@@ -13,39 +13,25 @@ are_xcode_command_line_tools_installed() {
 
 install_xcode_command_line_tools() {
   /usr/bin/touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
-  PROD=$( \
-    /usr/sbin/softwareupdate -l | \
-    grep --color=never -B 1 -E 'Command Line (Developer|Tools)' | \
-    awk -F'*' '/^ +\\*/ {print $2}' | \
-    grep '#{clt_macos_version}' | \
-    sort -V | \
-    sed 's/^ *//' | \
-    tail -n1 \
+
+  MACOS_VERSION=$( \
+    defaults read loginwindow SystemVersionStampAsString | \
+    /usr/bin/grep --color=never -o -E '^[0-9]+\.[0-9]+' \
   )
 
-  $DRY_RUN || sudo /usr/sbin/softwareupdate -v -i "$PROD"
-  $DRY_RUN || sudo /bin/rm -f /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
-  $DRY_RUN || sudo /usr/bin/xcode-select --switch /Library/Developer/CommandLineTools
+  PROD=$( \
+    sudo /usr/sbin/softwareupdate -l | \
+    /usr/bin/grep --color=never -B 1 -E 'Command Line (Developer|Tools)' | \
+    /usr/bin/awk -F'*' '/^ +\\*/ {print $2}' | \
+    /usr/bin/grep "${MACOS_VERSION}" | \
+    /usr/bin/sort -V | \
+    /usr/bin/sed 's/^ *//' | \
+    /usr/bin/tail -n1 \
+  )
 
-  # Fallback solution
-  ##############################################################################
-  # If necessary, prompt user to install
-  # the `Xcode Command Line Tools`.
-
-  # $DRY_RUN || sudo softwareupdate -i -a
-  # $DRY_RUN || xcode-select --install &> /dev/null
-
-  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-  # Wait until the `Xcode Command Line Tools` are installed.
-
-  # execute \
-  #     "until are_xcode_command_line_tools_installed; do \
-  #         sleep 5; \
-  #      done" \
-  #     "Xcode Command Line Tools"
-  ##############################################################################
-  # END Fallback solution
+  sudo /usr/sbin/softwareupdate --install "$PROD" > /dev/null
+  sudo /bin/rm -f /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+  sudo /usr/bin/xcode-select --switch /Library/Developer/CommandLineTools
 }
 
 are_xcode_command_line_tools_installed || install_xcode_command_line_tools
