71ecf36f5cd9b00c19ec17548bc43a486ff8d341
Patrick Eriksson <paleite@kth.se>
2019-04-11 12:42:59 +0200

    Fix clone script

--- dotfiles/clone.sh
+++ dotfiles/clone.sh
@@ -11,13 +11,14 @@ alias .f='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 git clone --depth 1 --bare https://github.com/paleite/dotfiles.git "${HOME}/.dotfiles/"
 
 cd "${HOME}"
-mkdir -p .dotfiles-backup
 
 if .f checkout; then
   echo "Checked out config.";
 else
   echo "Backing up pre-existing dot files.";
-  .f checkout 2>&1 | grep -E "\s+\." | awk '{print $1}' | xargs -I{} mv -v {} .dotfiles-backup/{}
+  mkdir -p .dotfiles-backup
+  readonly _FILES_TO_BACKUP=$(.f checkout 2>&1 || true)
+  cat <<< "${_FILES_TO_BACKUP[*]}" | grep -E "^\s+" | awk '{print $1}' | xargs -I{} mv -v "{}" ".dotfiles-backup/{}"
 fi;
 .f checkout
 .f config status.showUntrackedFiles no
