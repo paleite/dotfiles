49123895681102cef50b6a1a8a6e8d9bf08b9761
Patrick Eriksson <paleite@kth.se>
2019-04-11 16:41:47 +0200

    Move sudo-request

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -15,6 +15,12 @@ readonly TMPDIR=$(/usr/bin/dirname "$(/usr/bin/mktemp -u)")
 mkdir -p "$DIR"
 cd "$DIR"
 
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.macos` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 ################################################################################
 # macOS
 # TODO:
