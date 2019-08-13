723ec2715e9ec7452e0b9e647a7fec94ea778c73
Patrick Eriksson <patrick.eriksson@trustly.com>
2019-08-13 15:05:48 +0200

    Update cron tasks

--- cron/every-hour.sh
+++ cron/every-hour.sh
@@ -10,7 +10,15 @@ readonly DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 cd "${DIR}"
 # shellcheck source=cron/.functions
 source .functions
+source ~/.functions
 
-set -o verbose
+# set -o verbose
 
-exit 127
+_exit_on_tethered
+
+PROJECTS_LIST_FILE="${HOME}"/dev/.projects
+cd $(dirname "${PROJECTS_LIST_FILE}")
+
+for PROJECT_DIR in `grep -E '^[^#]' < "${PROJECTS_LIST_FILE}"`; do
+    check_upstream "${PROJECT_DIR}" &
+done < $PROJECTS_LIST_FILE
