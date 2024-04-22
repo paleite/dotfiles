77179ee53f018d0e48f4d8dbfb73c56c7276cea5
Patrick Eriksson <patrick@capchap.se>
2024-04-22 09:22:48 +0200

    Prevent Homebrew from being too outdated for 'brew bundle'

--- dotfiles/export-brewfile.sh
+++ dotfiles/export-brewfile.sh
@@ -7,4 +7,7 @@ set -o nounset
 
 echo "$(tput bold)dotfiles export-brewfile$(tput sgr0)"
 
+# Homebrew could be too outdated for `brew bundle`, so we need to update it
+# before running `brew bundle dump`
+brew update
 brew bundle dump --global --force --describe --verbose
